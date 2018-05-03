f7d9b1bdb716e2f2880bb332d3414be1718f8dbc
Jogi Silalahi <jsilalahi@dyned.com>
2018-05-03 12:30:40 +0700

    Added variables

--- hello-world/main.go
+++ hello-world/main.go
@@ -4,4 +4,13 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	var firstname string
+	firstname = "Jogi"
+
+	fmt.Println("Firstname: ", firstname)
+
+	lastname := "Silalahi"
+
+	fmt.Println("Lastname: ", lastname)
 }
